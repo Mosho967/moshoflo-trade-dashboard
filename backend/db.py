dc9b77cc750d41b61a01e5acda6dd170fe53f8c6
Mosho967 <Shoriray@gmail.com>
2025-07-16 22:18:35 +0100

    chore(db): set up SQLAlchemy engine, session, and Base

---
+++ backend/db.py
@@ -0,0 +1,13 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
+from backend.config import DATABASE_URL
+
+# Create DB engine
+engine = create_engine(DATABASE_URL)
+
+# Create a session factory
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+# Base class for models
+Base = declarative_base()
+
